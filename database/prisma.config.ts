100cffd3646b2a28f312d054aa13a2dfca24341a
Staylearner <rohanghosh078@gmail.com>
2026-05-04 11:57:57 +0530

    fix(prisma): load root .env in prisma config to resolve DATABASE_URL in monorepo setup

--- database/prisma.config.ts
+++ database/prisma.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig, env } from 'prisma/config';
-import "dotenv/config";
+import dotenv from 'dotenv';
+import * as path from 'path';
+
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
